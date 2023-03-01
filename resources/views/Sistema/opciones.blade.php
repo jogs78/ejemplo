f8df1c71fd65a91bc1470d990cdf1fa9879fca92
jogs78 <jogs78@gmail.com>
2023-03-01 13:25:15 -0600

    Pase a minusculas

--- resources/views/Sistema/opciones.blade.php
+++ resources/views/Sistema/opciones.blade.php
@@ -1,9 +1,9 @@
 @extends('plantillas.principal')
 @section('contenido')
 <ul>
-    <li><a href="{{route("saludar")}}">SALUDAR</a></li>
-    <li><a href="{{route("despedir")}}">DESPEDIR</a></li>
-    <li><a href="{{route("seleccionar")}}" >SELECCIONAR IDIOMA</a></li>
-    <li><a href="{{route("salida")}}" >SALIR</a></li>
+    <li><a href="{{route("saludar")}}">Saludar</a></li>
+    <li><a href="{{route("despedir")}}">Despedir</a></li>
+    <li><a href="{{route("seleccionar")}}" >Seleccionar Idioma</a></li>
+    <li><a href="{{route("salida")}}" >Salir</a></li>
 </ul>
 @endsection
