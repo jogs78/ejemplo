001ca3061e562e8e5457301b276f75574eacd8b9
Erick <erickgp51@gmail.com>
2023-03-01 13:56:49 -0600

    agregue punto punto

--- resources/views/Sistema/opciones.blade.php
+++ resources/views/Sistema/opciones.blade.php
@@ -1,9 +1,9 @@
 @extends('plantillas.principal')
 @section('contenido')
-<ul>
-    <li><a href="{{route("saludar")}}">Saludar.</a></li>
-    <li><a href="{{route("despedir")}}">Despedir.</a></li>
-    <li><a href="{{route("seleccionar")}}" >Seleccionar Idioma.</a></li>
-    <li><a href="{{route("salida")}}" >Salir.</a></li>
-</ul>
+    <ul>
+        <li><a href="{{ route('saludar') }}">Saludar..</a></li>
+        <li><a href="{{ route('despedir') }}">Despedir..</a></li>
+        <li><a href="{{ route('seleccionar') }}">Seleccionar Idioma..</a></li>
+        <li><a href="{{ route('salida') }}">Salir..</a></li>
+    </ul>
 @endsection
