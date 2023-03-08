131426c237056509f7941925ea787b6e539244e7
Erick <erickgp51@gmail.com>
2023-03-08 13:47:26 -0600

    comentario erick

--- routes/web.php
+++ routes/web.php
@@ -22,24 +22,24 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('login',[SistemaController::class,'entrada'])->name("login");
-Route::post('validar',[SistemaController::class, 'validar'])->name('validar');
-Route::get('ropciones',[SistemaController::class, 'mopciones'])->name('nopciones');
+Route::get('login', [SistemaController::class, 'entrada'])->name("login");
+Route::post('validar', [SistemaController::class, 'validar'])->name('validar');
+Route::get('ropciones', [SistemaController::class, 'mopciones'])->name('nopciones');
 
-Route::get('seleccionar',[IdiomaControler::class, 'seleccionar'])->name("seleccionar");
-Route::post('procesar',[IdiomaControler::class, 'procesar'])->name("procesar");
+Route::get('seleccionar', [IdiomaControler::class, 'seleccionar'])->name("seleccionar");
+Route::post('procesar', [IdiomaControler::class, 'procesar'])->name("procesar");
 
-Route::get('saludar',[IdiomaControler::class, 'saludar'])->name("saludar")->middleware("checaridioma");
-Route::get('despedir',[IdiomaControler::class, 'despedir'])->name("despedir")->middleware("checaridioma");
+Route::get('saludar', [IdiomaControler::class, 'saludar'])->name("saludar")->middleware("checaridioma");
+Route::get('despedir', [IdiomaControler::class, 'despedir'])->name("despedir")->middleware("checaridioma");
 
-Route::get('salir',[SistemaController::class, 'salir'])->name('salida');
+Route::get('salir', [SistemaController::class, 'salir'])->name('salida');
 
 
-Route::get('paquetes',[PaqueteController::class, 'index'])->name('paquetes.index');
-Route::get('crearpaquete',[PaqueteController::class, 'create'])->name('paquetes.create');
-Route::post('guardar',[PaqueteController::class, 'store'])->name('paquetes.store');
-Route::get('actualizar/{cual?}',[PaqueteController::class, 'edit'])->name('paquetes.edit');
-Route::put('actualizar/{cual?}',[PaqueteController::class, 'update'])->name('paquetes.update');
-Route::delete('borrar/{cual?}',[PaqueteController::class, 'destroy'])->name('paquetes.destroy');
-
+Route::get('paquetes', [PaqueteController::class, 'index'])->name('paquetes.index');
+Route::get('crearpaquete', [PaqueteController::class, 'create'])->name('paquetes.create');
+Route::post('guardar', [PaqueteController::class, 'store'])->name('paquetes.store');
+Route::get('actualizar/{cual?}', [PaqueteController::class, 'edit'])->name('paquetes.edit');
+Route::put('actualizar/{cual?}', [PaqueteController::class, 'update'])->name('paquetes.update');
+Route::delete('borrar/{cual?}', [PaqueteController::class, 'destroy'])->name('paquetes.destroy');
+//comentario Erick 
 Route::resource('servicios', PaqueteController::class);
